03a9684da06f0c96731a63b61e1518b89db6cad7
sinee-nebushko <ikiwik1206@gmail.com>
2018-10-24 02:57:42 +0300

    random

--- модуль рандом.py
+++ модуль рандом.py
@@ -1,9 +1,9 @@
 import random
-a=random.randint(1,10)
+a = random.randint(1, 10)
 print(a)
-b=int(input('Компьютер загадал число от 1 до 10.\n Ваш вариант : '))
+b = int(input('Компьютер загадал число от 1 до 10.\n Ваш вариант : '))
 while a!=b:
-    b=int(input('продолжай '))
+    b = int(input('продолжай '))
     continue
 print('Ура! Правильный ответ - ', b)
 
