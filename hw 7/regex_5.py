7df4c0c27d4e3ce3b75e8224cc0b292fe7686f04
sinee-nebushko <ikiwik1206@gmail.com>
2018-12-07 20:05:02 +0300

    Homework

---
+++ hw 7/regex_5.py
@@ -0,0 +1,6 @@
+from re import fullmatch
+
+p = ['a', 'an', 'the']
+str = '''Now you can watch the video again and test your listening skills without the text.
+      Reading is a very important language learning skill. 
+      It helps you improve all parts of the English language – vocabulary, spelling, grammar, and writing.'''
