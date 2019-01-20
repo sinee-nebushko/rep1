810548c609c324edfe2e3c747dc84704b2da86d3
sinee-nebushko <ikiwik1206@gmail.com>
2019-01-20 21:12:53 +0300

    voronoi

--- hw 8/voronoi_noise_mult.py
+++ hw 8/voronoi_noise_mult.py
@@ -1,38 +1,44 @@
 from threading import Thread
 import math
 import random
-from PIL import Image, ImageDraw
+from PIL import Image
 imgx = 900
 imgy = 500
-image = Image.new("L", (imgx, imgy),0)
-draw = ImageDraw.Draw(image)
+image = Image.new("L", (imgx, imgy))
 pixels = image.load()
-n = random.randint(0, 100)  # random number of seed points
-m = 0
+n = random.randint(0, 100)
+
 seedsX = [random.randint(0, imgx - 1) for i in range(n)]
 seedsY = [random.randint(0, imgy - 1) for i in range(n)]
-maxDist = 0.0
+
+imgx1 = [int(i) for i in range(0, 300)]
+imgx2 = [int(i) for i in range(300, 600)]
+imgx3 = [int(i) for i in range(600, 900)]
+imgy1 = [int(i) for i in range(0, 500)]
 
 
-def f(maxDist):
-    for ky in range(imgy):
-        for kx in range(imgx):
-            dists = [math.hypot(seedsX[i] - kx, seedsY[i] - ky) for i in
-                     range(n)]  # list of distances to all seed points
+def f(x, y):
+    max_dist = 0.0
+    m = 0
+    for ky in y:
+        for kx in x:
+            dists = [math.hypot(seedsX[i] - kx, seedsY[i] - ky) for i in range(n)]
             dists.sort()
-            if dists[m] > maxDist:
-                maxDist = dists[m]  # find max distance
-            pixels[kx, ky] = int(round(255 * (dists[m] / maxDist)))
+            if dists[m] > max_dist:
+                max_dist = dists[m]
+            pixels[kx, ky] = int(round(255 * (dists[m] / max_dist)))
 
 
-thread1 = Thread(target=f, args=(imgx, imgy))
-thread2 = Thread(target=f)
-thread3 = Thread(target=f)
+thread1 = Thread(target=f, args=(imgx1, imgy1))
+thread2 = Thread(target=f, args=(imgx2, imgy1))
+thread3 = Thread(target=f, args=(imgx3, imgy1))
+
 thread1.start()
 thread2.start()
 thread3.start()
+
 thread1.join()
 thread2.join()
 thread3.join()
 
-image.save("WorleyNoiseIM12.png", "PNG")
+image.save("voronoi_noise_mult.png", "PNG")
